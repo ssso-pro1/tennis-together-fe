8f204006ee0121b8795f30b9e8a43a85cd3385b5
Song da young <88611957+didi2578@users.noreply.github.com>
2021-10-28 22:18:19 +0900

    🐛 Fix a branch

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React from 'react'
 import Writing from './Writing'
 import DetailMain from './DetailMain'
 import './Styles/main.scss'
+import './App.css'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import ListPage from './ListPage'
+import { useState } from 'react'
 
 import { Link, Route, Switch } from 'react-router-dom'
 
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import ReactDOM from 'react-dom';
-import App from './App';
-import 'antd/dist/antd.css';
-import { BrowserRouter } from 'react-router-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import 'antd/dist/antd.css'
+import reportWebVitals from './reportWebVitals'
 
 ReactDOM.render(
-  <>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
-);
+)
