8798a8d02b0fb2af6c847ef9927f4399388c599f
Soyeon Jang <ssso.pro1@gmail.com>
2021-11-04 01:38:14 +0900

    add : navbar change by uid
    - uid(임의) 유무에 따른 navbar 변화 (로그인버튼과 아바타)
    - 로고 클릭 시 메인페이지로 이동 (히스토리)
    - 로그인 클릭 시 로그인 페이지로 이동 (히스토리)
    - delete : Navbar.module.css -> styled.components 로 변경
    - navbar drop menu 시도 (적용 안됨)

---
+++ src/components/Common/DropMenu.js
@@ -0,0 +1,68 @@
+// import React from 'react'
+// import styled from 'styled-components'
+
+// import { Menu, Dropdown } from 'antd'
+// import { DownOutlined } from '@ant-design/icons'
+
+// const DropMenu = {
+//     (
+//   <Menu>
+//     <Menu.Item>
+//       <a
+//         target="_blank"
+//         rel="noopener noreferrer"
+//         href="https://www.antgroup.com"
+//       >
+//        내가 쓴 글
+//       </a>
+//     </Menu.Item>
+//     <Menu.Item>
+//       <a
+//         target="_blank"
+//         rel="noopener noreferrer"
+//         href="https://www.aliyun.com"
+//       >
+//         히스토리
+//       </a>
+//     </Menu.Item>
+//     <Menu.Item>
+//       <a
+//         target="_blank"
+//         rel="noopener noreferrer"
+//         href="https://www.luohanacademy.com"
+//       >
+//         알림
+//       </a>
+//     </Menu.Item>
+//     <Menu.Item>
+//     <a
+//       target="_blank"
+//       rel="noopener noreferrer"
+//       href="https://www.antgroup.com"
+//     >
+//      친구목록
+//     </a>
+//   </Menu.Item>
+//   <Menu.Item>
+//   <a
+//     target="_blank"
+//     rel="noopener noreferrer"
+//     href="https://www.antgroup.com"
+//   >
+//    로그아웃
+//   </a>
+// </Menu.Item>
+//     <Menu.Item danger>a danger item</Menu.Item>
+//   </Menu>
+//     );
+
+// return (
+//   <Dropdown overlay={menu}>
+//     <a className="ant-dropdown-link" onClick={(e) => e.preventDefault()}>
+//       Hover me <DownOutlined />
+//     </a>
+//   </Dropdown>
+// )
+// }
+
+// export default DropMenu
--- src/components/ListPage/ListPage.jsx
+++ src/components/ListPage/ListPage.jsx
@@ -23,11 +23,37 @@ const ListPage = ({ games, games: { content } }) => {
     history.push(`/detail/${game.game_no}`)
   }
 
+  // const GamesList = styled.section`
+  //   display: flex;
+  //   flex-direction: row;
+  // `
+
   return (
     <>
       <Navbar />
       <Header />
       <Section>
+        {/* <section>
+          <div>
+            <h3>검색하기</h3>
+            <Search />
+          </div>
+          <div>
+            <h3>현재 가능한 경기</h3>
+            <GamesList>
+              <ul>
+                {games &&
+                  content.map((game) => (
+                    <ItemPage //
+                      key={game.game_no}
+                      game={game}
+                      onGameClick={onGameClick}
+                    />
+                  ))}
+              </ul>
+            </GamesList>
+          </div>
+        </section> */}
         <section className={styles.listpage}>
           <div className={styles.searchDiv}>
             <h3 className={styles.title}>검색하기</h3>
