cbfc4213db33e71c25b4d1d4e302e94cec3bebc8
Soyeon Jang <ssso.pro1@gmail.com>
2021-11-21 17:53:17 +0900

    add : UpdateProfile, footer
    - 프로필 수정 파일 추가
    - 메인페이지 푸터바 추가
    - 불필요한 route삭제

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import React, { useContext } from 'react'
+import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
-import { UserContext } from 'service/authState'
 
 import Writing from './components/Writing/Writing'
 import DetailMain from './components/Detail/DetailMain'
@@ -11,14 +10,13 @@ import AuthState from 'service/authState'
 import EditForm from 'components/Writing/EditForm'
 import MyHistory from 'MyPage/MyHistory'
 import Notifications from 'MyPage/Notifications'
-import ReviewList from 'components/PopUpProfile/ReviewList'
-import PopUpProfile from 'components/PopUpProfile/PopUpProfile'
 
 import GlobalStyle from './styled-components/GlobalStyles'
 
 import theme from './styled-components/theme'
 import styled, { ThemeProvider } from 'styled-components'
 import font from '../src/styled-components/font.css'
+import UpdateProfile from 'MyPage/UpdateProfile'
 
 function App() {
   return (
@@ -33,7 +31,6 @@ function App() {
             </Route>
             <Route path="/authin">
               <AuthPage />
-              {/* <AuthPage /> */}
             </Route>
             <Route path="/signup">
               <SignUpPage />
@@ -47,12 +44,9 @@ function App() {
             <Route path="/editing/:gameNo">
               <EditForm />
             </Route>
-            {/* <Route path="/reviewlist">
-              <ReviewList />
+            {/* <Route path="/updateprofile">
+              <UpdateProfile />
             </Route> */}
-            <Route path="/popup">
-              <PopUpProfile />
-            </Route>
             <Route path="/history">
               <MyHistory />
             </Route>
