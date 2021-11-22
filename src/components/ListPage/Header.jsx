7ebb9845831f91da494fdb8a3d81a66e36e9e4ee
Soyeon Jang <ssso.pro1@gmail.com>
2021-11-22 12:48:37 +0900

    add : anchor, pagination

--- src/components/ListPage/Header.jsx
+++ src/components/ListPage/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Slider from 'react-slick'
 import styled from 'styled-components'
+import { useHistory } from 'react-router-dom'
 // import Slider from '@ant-design/react-slick'
 // import '~slick-carousel/slick/slick.css'
 // import '~slick-carousel/slick/slick-theme.css'
@@ -18,6 +19,8 @@ const Header = (props) => {
 
   //   header 사진 사이즈 고정? 디자인 구체적으로 다시 정하기
   const Container = styled.div`
+    scroll-behavior: smooth;
+
     box-sizing: border-box;
     width: 1200px;
     height: 25em;
@@ -35,11 +38,20 @@ const Header = (props) => {
         width: 100%;
         height: 100%;
       }
-      .banner {
+      .banner2,
+      .banner3 {
         cursor: pointer;
+      }
+      .banner2 {
+        scroll-behavior: smooth;
       }
     }
   `
+  const history = useHistory()
+
+  const goToWriting = () => {
+    history.push('/writing')
+  }
 
   return (
     <Container>
@@ -47,10 +59,12 @@ const Header = (props) => {
         <div>
           <img src="/images/banner/banner11.png" alt="header-img" />
         </div>
-        <div className="banner">
-          <img src="/images/banner/banner2.png" alt="header-img" />
+        <div className="banner2">
+          <a href="#searchA">
+            <img src="/images/banner/banner2.png" alt="header-img" />
+          </a>
         </div>
-        <div>
+        <div className="banner3" onClick={() => goToWriting()}>
           <img src="/images/banner/banner3.png" alt="header-img" />
         </div>
       </Slider>
