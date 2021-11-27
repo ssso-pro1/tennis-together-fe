f88518c36c6a8f262566febffec3be509b1527b8
Soyeon Jang <ssso.pro1@gmail.com>
2021-11-27 21:24:46 +0900

    add : add a friend by clicking popupprofile button
    팝업프로필에 친구추가 버튼 클릭 시 친구 추가 기능 구현

--- src/components/Friends/FriendsList.js
+++ src/components/Friends/FriendsList.js
@@ -12,16 +12,14 @@ import Button from 'styled-components/Buttons'
 import { Input, Rate, Select } from 'antd'
 import FriendItem from './FriendItem'
 
-const FriendsList = ({ props }) => {
+const FriendsList = ({ children }) => {
   const history = useHistory()
   history.push('/pages/friends')
   const { user } = useContext(UserContext)
+
   const [friends, setFriends] = useState(null)
+
   const uid = user && user.uid
-  // if (user) {
-  //   console.log(user)
-  //   console.log(user.uid)
-  // }
 
   useEffect(() => {
     baseApi
@@ -66,50 +64,50 @@ const FriendsList = ({ props }) => {
 
   if (!user) return <></>
   if (!friends) return <></>
-  // console.log(user)
-
-  // console.log(user.uid)
 
   return (
     <>
-      <Navbar />
-      {/* {user && ( */}
-      <div>
-        <Flexbox
-          className="mypage-header"
-          style={{
-            height: '70px',
-            borderBottom: '1px solid lightgrey',
-            marginBottom: '50px',
-          }}
-        >
-          <h2 style={{ fontWeight: '700', fontSize: '20px', width: '25%' }}>
-            친구목록
-          </h2>
-          <div style={{ width: '50%' }}>
-            <Input
-              className="input"
-              style={{ width: 500 }}
-              placeholder="닉네임 검색하기"
-            />
-          </div>
-          <Button Secondary height={'30px'} width={'60px'} fs={'14px'}>
-            검색
-          </Button>
-        </Flexbox>
-        {/* // )} */}
-        <Section>
-          <Profile className="profileDiv" />
-          <ul className="FriendDiv">
-            {friends &&
-              friends.map((friend) => (
-                // <FriendItem key={friend.frdRelNo} friend={friend.frdUser} />
-                <FriendItem key={friend.frdRelNo} friend={friend} />
-              ))}
-          </ul>
-        </Section>
-      </div>
-      {/* )} */}
+      <UserContext.Provider value={{ friends, setFriends }}>
+        {children}
+
+        <Navbar />
+        {/* {user && ( */}
+        <div>
+          <Flexbox
+            className="mypage-header"
+            style={{
+              height: '70px',
+              borderBottom: '1px solid lightgrey',
+              marginBottom: '50px',
+            }}
+          >
+            <h2 style={{ fontWeight: '700', fontSize: '20px', width: '25%' }}>
+              친구목록
+            </h2>
+            <div style={{ width: '50%' }}>
+              <Input
+                className="input"
+                style={{ width: 500 }}
+                placeholder="닉네임 검색하기"
+              />
+            </div>
+            <Button Secondary height={'30px'} width={'60px'} fs={'14px'}>
+              검색
+            </Button>
+          </Flexbox>
+          {/* // )} */}
+          <Section>
+            <Profile className="profileDiv" />
+            <ul className="FriendDiv">
+              {friends &&
+                friends.map((friend) => (
+                  <FriendItem key={friend.frdRelNo} friend={friend} />
+                ))}
+            </ul>
+          </Section>
+        </div>
+        {/* )} */}
+      </UserContext.Provider>
     </>
   )
 }
