de560af7863ccb889ec47b806d38cbda70c98e43
Soyeon Jang <ssso.pro1@gmail.com>
2021-12-08 16:50:12 +0900

    fix : initialValue -> placeholder, clean : unused lines

--- src/MyPage/UpdateProfile.js
+++ src/MyPage/UpdateProfile.js
@@ -5,31 +5,58 @@ import { UserContext } from '../service/authState'
 import baseApi from '../service/baseApi'
 import { Form, Select, Input, Upload, message } from 'antd'
 import { UploadOutlined } from '@ant-design/icons'
-import { historyType, locSdData, locSkkData } from 'components/Common/constants'
+import { historyT, locSdData, locSkkData } from 'components/Common/constants'
 import styled from 'styled-components'
 import AvatarBase from 'styled-components/AvatarBase'
 import DefaultImg from 'styled-components/assets/images/img-user-default.png'
 import Button from 'styled-components/Buttons'
+
+// style
+const Flexbox = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  h2 {
+    font-size: 1.8rem;
+    margin: 5rem;
+  }
+  .avatar {
+    display: flex;
+    justify-content: center;
+    img {
+      width: 5rem;
+    }
+  }
+`
+
+const SignUpSection = styled.div`
+  padding-bottom: 20%;
+`
+
+const InputData = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+const Birth = styled.div``
+
+const Nickname = styled.div`
+  display: flex;
+`
 
 const UpdateProfile = () => {
   const history = useHistory()
   history.push('/pages/updateprofile')
   const { user, setUser } = useContext(UserContext)
 
-  const [phoneNumber, setPhoneNumber] = useState(null)
-  const [profileUrl, setProfileUrl] = useState(null)
-  // const [nickname, setNickname] = useState(null)
-  // const [birth, setBirth] = useState(null)
-  // const [gender, setGender] = useState(null)
-  // const [history, setHistory] = useState(null)
-  // const [locSd, setLocSd] = useState(null)
-  // const [locSkk, setLocSkk] = useState(null)
-
   const { Option } = Select
   const [form] = Form.useForm()
 
   const [locSds, setLocSds] = React.useState(locSdData[0].value)
   const [locSkks, setLocSkks] = React.useState(locSkkData[locSds][0].value)
+  const [phoneNumber, setPhoneNumber] = useState(null)
+  const [profileUrl, setProfileUrl] = useState(null)
+  const [nickInput, setNickInput] = useState(null)
 
   const handleLocSdChange = (value) => {
     setLocSds(value)
@@ -46,56 +73,67 @@ const UpdateProfile = () => {
     console.log(phoneNumber)
   }
 
+  useEffect(() => {
+    console.log(profileUrl)
+    // setProfileUrl()
+  }, [profileUrl])
+
+  // 닉네임 수정 시 set
+  const handleNick = (e) => {
+    e.preventDefault()
+    const nick = e.target.value
+    setNickInput(nick)
+    console.log('닉수정', nickInput)
+  }
+
+  // 닉네임 중복확인
+  const confirmNick = (e) => {
+    e.preventDefault()
+    console.log('닉네임중복확인 (api)')
+    baseApi
+      .get(
+        `/users/nickname/${nickInput}`,
+        {
+          nickname: nickInput,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem('token')}`,
+          },
+        }
+      )
+      .then(function (response) {
+        console.log(response)
+        console.log('response된닉', nickInput)
+        alert('사용가능한 닉네임입니다.')
+        setNickInput(nickInput)
+      })
+      .catch((error) => {
+        console.log(error)
+        console.log('수정실패된 nick', nickInput)
+        alert('이미 사용중인 닉네임입니다.')
+      })
+  }
+
   const uid = user && user.uid
 
-  // ant file upload 1
-  /*
-  const changeImg = {
-    name: 'file',
-    action: '/profile/pic',
-    // body: {
-    //   uid: 'uid',
-    //   file: user.profileUrl,
-    // },
-    headers: {
-      authorization: `Bearer ${localStorage.getItem('token')}`,
-    },
-    onChange(info) {
-      if (info.file.status !== 'uploading') {
-        console.log(info.file, info.fileList)
-      }
-      if (info.file.status === 'done') {
-        const formData = new FormData()
-        formData.append('file', info.fileList[0])
-        formData.append('uid', uid)
-        baseApi.post('/profile/pic', formData)
-
-        message.success(`${info.file.name} file uploaded successfully`)
-        // setProfileUrl(profileUrl)
-      } else if (info.file.status === 'error') {
-        message.error(`${info.file.name} file upload failed.`)
-      }
-    },
-  }
-  */
-
-  // ant file upload 2
+  // 이미지 업로드
   const changeImg = {
     name: 'file',
     action: '/profile/pic',
     data: {
       uid: uid,
     },
-
     onChange(info) {
       if (info.file.status !== 'uploading') {
         console.log(info.file, info.fileList)
       }
       if (info.file.status === 'done') {
         message.success(`${info.file.name} file uploaded successfully`)
-        // setProfileUrl(profileUrl)
+        setProfileUrl(info.file)
       } else if (info.file.status === 'error') {
         message.error(`${info.file.name} file upload failed.`)
+        console.log(uid)
       }
     },
   }
@@ -107,13 +145,11 @@ const UpdateProfile = () => {
         `/users/${uid}`,
         {
           phone: phoneNumber,
-          nickname: values.nickname,
+          nickname: nickInput,
           birth: values.birth,
           gender: values.gender,
           history: parseInt(values.history),
-          // profileUrl: profileUrl,
-          // locSd: values.locSd,
-          // locSkk: values.locSkk,
+          profileUrl: profileUrl && profileUrl.uid, //수정성공인데 선택 안하고 클릭 시 위의 에러뜸
           locSd: values.locSd.toString(),
           locSkk: values.locSkk.toString(),
         },
@@ -126,6 +162,7 @@ const UpdateProfile = () => {
       .then(function (response) {
         console.log(response)
         console.log('수정완료된 vlaues', values)
+        console.log('수정요청한 닉넴', nickInput)
         alert('프로필 수정이 완료되었습니다.')
         setUser(user)
         history.push('/pages/updateprofile')
@@ -137,48 +174,10 @@ const UpdateProfile = () => {
       })
   }
 
-  const confirmNick = () => {
-    console.log('닉네임중복확인 (api)')
-  }
-
-  // style
-  const Flexbox = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    h2 {
-      font-size: 1.8rem;
-      margin: 5rem;
-    }
-    .avatar {
-      display: flex;
-      justify-content: center;
-      img {
-        width: 5rem;
-      }
-    }
-  `
-
-  const SignUpSection = styled.div`
-    Input {
-      width: 80%;
-    }
-    Button {
-      width: 20%;
-    }
-    padding-bottom: 20%;
-  `
-
-  const InputData = styled.div`
-    display: flex;
-    flex-direction: column;
-  `
-  const Birth = styled.div``
-
-  const Nickname = styled.div`
-    display: flex;
-  `
+  if (!user) return <></>
+  console.log('user', user)
+  // console.log(profileUrl)
+  // console.log('프로필사진', user.profileUrl)
 
   return (
     <>
@@ -186,7 +185,22 @@ const UpdateProfile = () => {
         <SignUpSection>
           <h2>프로필 수정</h2>
           <AvatarBase className="avatar">
-            <img src={DefaultImg} alt={DefaultImg} />
+            {user.profileUrl ? (
+              <img
+                className="avatarImg"
+                // style={{ width: '2.2rem' }}
+                src={user.profileUrl}
+                alt=""
+              />
+            ) : (
+              <img
+                className="avatarImg"
+                // style={{ width: '2.2rem' }}
+                src={DefaultImg}
+                alt={DefaultImg}
+              />
+            )}
+            {/* <img src={DefaultImg} alt={DefaultImg} /> */}
           </AvatarBase>
 
           <Upload {...changeImg}>
@@ -194,47 +208,59 @@ const UpdateProfile = () => {
               icon={<UploadOutlined />}
               Secondary
               height={'30px'}
-              width={'100px'}
-              style={{ fontSize: '10px', fontWeight: '400' }}
+              width={'70px'}
+              style={{ fontSize: '8px', fontWeight: '100' }}
               // onClick={ChangeImg}
             >
               사진 선택
             </Button>
           </Upload>
-
+          <br />
           <InputData>
             <Form onFinish={onFinish} form={form}>
-              {/* initialValues={losCd} */}
+              <Nickname>
+                {/* <Form.Item name="nickname" initialValue={user.nickname}> */}
+                <Form.Item name="nickname" initialValue={user.nickname}>
+                  <Input
+                    type="text"
+                    name="nickname"
+                    placeholder={nickInput}
+                    style={{ width: 220 }}
+                    onChange={handleNick}
+                  />
+                </Form.Item>
+                <Button
+                  height={'32px'}
+                  width={'80px'}
+                  htmlType="button"
+                  Outlined
+                  style={{ fontSize: '12px', fontWeight: '400' }}
+                  onClick={(e) => {
+                    confirmNick(e)
+                  }}
+                  // onClick={confirmNick}
+                >
+                  중복확인
+                </Button>
+              </Nickname>
 
-              <Form.Item
-                name="locSd"
-                initialValue={locSdData[user.locCd.locSd].value}
-              >
+              <Form.Item name="locSd">
                 <Select
-                  // placeholder={<span>시/도</span>}
+                  placeholder={user.locCd.locSdName}
                   style={{ width: 300 }}
                   onChange={handleLocSdChange}
                 >
                   {locSdData.map((locSd) => (
-                    <Option
-                      key={locSd.value}
-                      value={locSd.value}
-                      // name={locSd.name}
-                    >
+                    <Option key={locSd.value} value={locSd.value}>
                       {locSd.name}
                     </Option>
                   ))}
                 </Select>
               </Form.Item>
 
-              <Form.Item
-                name="locSkk"
-                initialValue={locSkkData[locSds][user.locCd.locSkk].value}
-                // initialValue={locSkkData[user.locCd.locSkk][locSds].value}
-              >
+              <Form.Item name="locSkk">
                 <Select
-                  // defaultValue={locSkkData[locSds][user.locCd.locSkk].value}
-                  // placeholder={<span>군/구</span>}
+                  placeholder={user.locCd.locSkkName}
                   style={{ width: 300 }}
                   onChange={handleLocSkkChange}
                 >
@@ -246,9 +272,9 @@ const UpdateProfile = () => {
                 </Select>
               </Form.Item>
 
-              <Form.Item name="gender" initialValue={user.gender}>
+              <Form.Item name="gender">
                 <Select
-                  placeholder={<span>성별</span>}
+                  placeholder={user.gender}
                   style={{ width: 300 }}
                   required
                 >
@@ -261,12 +287,9 @@ const UpdateProfile = () => {
                 </Select>
               </Form.Item>
 
-              <Form.Item
-                name="history"
-                initialValue={historyType[user.history]}
-              >
+              <Form.Item name="history">
                 <Select
-                  placeholder={<span>경력</span>}
+                  placeholder={historyT[user.history]}
                   style={{ width: 300, height: 40 }}
                 >
                   <Option name="history" value="1">
@@ -284,39 +307,24 @@ const UpdateProfile = () => {
                 </Select>
               </Form.Item>
 
-              <Form.Item name="birth" initialValue={user.birth}>
+              <Form.Item name="birth">
                 <Input
                   type="text"
                   name="birth"
-                  placeholder="생년월일(900327)"
+                  placeholder={user.birth}
                   style={{ width: 300, height: 40 }}
                 />
               </Form.Item>
 
-              <Form.Item name="phone" initialValue={user.phone}>
+              <Form.Item name="phone">
                 <Input
                   type="text"
                   name="phone"
-                  placeholder="핸드폰 번호(01012341234)"
+                  placeholder={user.phone}
                   onChange={handlePhone}
                   style={{ width: 300, height: 40 }}
                 />
               </Form.Item>
-
-              <Nickname>
-                <Form.Item name="nickname" initialValue={user.nickname}>
-                  <Input type="text" name="nickname" placeholder="닉네임" />
-                </Form.Item>
-                <Button
-                  Secondary
-                  style={{ fontSize: '12px', fontWeight: '400' }}
-                  onClick={() => {
-                    confirmNick()
-                  }}
-                >
-                  중복확인
-                </Button>
-              </Nickname>
               <br />
 
               <Form.Item>
