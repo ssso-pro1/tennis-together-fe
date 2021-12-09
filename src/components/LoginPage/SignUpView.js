b7ab2c9f9f989abb0b598a4786a74f0621612bf5
Soyeon Jang <ssso.pro1@gmail.com>
2021-12-09 20:36:15 +0900

    clean : clean lines

--- src/components/LoginPage/SignUpView.js
+++ src/components/LoginPage/SignUpView.js
@@ -16,7 +16,40 @@ const SignUpView = () => {
   //       console.log(response)
   //     })
   // }, [])
+
   const { Option } = Select
+
+  // style
+  const Flexbox = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    h2 {
+      font-size: 1.8rem;
+      margin: 5rem;
+    }
+  `
+
+  const SignUpSection = styled.div`
+    Input {
+      width: 80%;
+    }
+    Button {
+      width: 20%;
+    }
+    padding-bottom: 20%;
+  `
+
+  const InputData = styled.div`
+    display: flex;
+    flex-direction: column;
+  `
+  const Birth = styled.div``
+
+  const Nickname = styled.div`
+    display: flex;
+  `
 
   const history = useHistory()
   history.push('/pages/signup')
@@ -24,10 +57,7 @@ const SignUpView = () => {
   const historyState = history.location.state
   const [form] = Form.useForm()
   const [user, setUser] = useState(historyState && historyState.id)
-
-  const confirmNick = () => {
-    console.log('닉네임중복확인하는 함수넣기 (api)')
-  }
+  const [nickInput, setNickInput] = useState(null)
 
   /*
   axios
@@ -90,15 +120,63 @@ const SignUpView = () => {
     setCBirth(birthday)
   }
 */
+
+  const [locSds, setLocSds] = React.useState(locSdData[0].value)
+  const [locSkks, setLocSkks] = React.useState(locSkkData[locSds][0].value)
+
+  const handleLocSdChange = (value) => {
+    setLocSds(value)
+  }
+
+  const handleLocSkkChange = (value) => {
+    setLocSkks(value)
+  }
+
+  // 닉네임 수정 시 set
+  const handleNick = (e) => {
+    e.preventDefault()
+    const nick = e.target.value
+    setNickInput(nick)
+    console.log(nick)
+  }
+
+  // 닉네임 중복확인
+  const confirmNick = () => {
+    console.log('닉네임중복확인 (api)')
+    baseApi
+      .get(
+        `/users/nickname/${nickInput}`,
+        {
+          nickname: nickInput,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem('token')}`,
+          },
+        }
+      )
+      .then(function (response) {
+        console.log(response)
+        console.log('response된닉', nickInput)
+        alert('사용가능한 닉네임입니다.')
+        setNickInput(nickInput)
+      })
+      .catch((error) => {
+        console.log(error)
+        console.log('수정실패된 nick', nickInput)
+        alert('이미 사용중인 닉네임입니다.')
+      })
+  }
+
   const onFinish = async (values) => {
-    console.log(values) //heroku 로 가입 시도 시 여기까지 출력되고 catch error
-    // console.log(localStorage.getItem('token'), '회원가입에넘기는토큰')
+    console.log(values)
+    console.log(localStorage.getItem('token'), '회원가입에넘기는토큰')
     baseApi
       .post(
         '/users',
         {
           phone: historyState.phone,
-          nickname: values.nickName,
+          nickname: values.nickName, //nickINput
           birth: values.birth,
           gender: values.gender,
           history: parseInt(values.history),
@@ -126,132 +204,37 @@ const SignUpView = () => {
       })
   }
 
-  // const locSdData = [
-  //   {
-  //     id: 1,
-  //     label: '서울시',
-  //     name: '서울시',
-  //     value: 1,
-  //   },
-  //   {
-  //     id: 2,
-  //     label: '경기도',
-  //     name: '경기도',
-  //     value: 2,
-  //   },
-  // ]
-
-  // const locSkkData = {
-  //   1: [
-  //     { label: '종로구', name: '종로구', value: 1 },
-  //     { label: '중구', name: '중구', value: 2 },
-  //     { label: '용산구', name: '용산구', value: 3 },
-  //     { label: '성동구', name: '성동구', value: 4 },
-  //     { label: '광진구', name: '광진구', value: 5 },
-  //     { label: '동대문구', name: '동대문구', value: 6 },
-  //     { label: '중랑구', name: '중랑구', value: 7 },
-  //     { label: '성북구', name: '성북구', value: 8 },
-  //     { label: '강북구', name: '강북구', value: 9 },
-  //     { label: '도봉구', name: '도봉구', value: 10 },
-  //     { label: '노원구', name: '노원구', value: 11 },
-  //     { label: '은평구', name: '은평구', value: 12 },
-  //     { label: '서대문구', name: '서대문구', value: 13 },
-  //     { label: '마포구', name: '마포구', value: 14 },
-  //     { label: '양천구', name: '양천구', value: 15 },
-  //     { label: '강서구', name: '강서구', value: 16 },
-  //     { label: '구로구', name: '구로구', value: 17 },
-  //     { label: '금천구', name: '금천구', value: 18 },
-  //     { label: '영등포구', name: '영등포구', value: 19 },
-  //     { label: '동작구', name: '동작구', value: 20 },
-  //     { label: '관악구', name: '관악구', value: 21 },
-  //     { label: '서초구', name: '서초구', value: 22 },
-  //     { label: '강남구', name: '강남구', value: 23 },
-  //     { label: '송파구', name: '송파구', value: 24 },
-  //     { label: '강동구', name: '강동구', value: 25 },
-  //   ],
-  //   2: [
-  //     { label: '수원시', name: '수원시', value: 1 },
-  //     { label: '성남시', name: '성남시', value: 2 },
-  //     { label: '고양시', name: '고양시', value: 3 },
-  //     { label: '용인시', name: '용인시', value: 4 },
-  //     { label: '부천시', name: '부천시', value: 5 },
-  //     { label: '안산시', name: '안산시', value: 6 },
-  //     { label: '안양시', name: '안양시', value: 7 },
-  //     { label: '남양주시', name: '남양주시', value: 8 },
-  //     { label: '화성시', name: '화성시', value: 9 },
-  //     { label: '평택시', name: '평택시', value: 10 },
-  //     { label: '의정부시', name: '의정부시', value: 11 },
-  //     { label: '시흥시', name: '시흥시', value: 12 },
-  //     { label: '파주시', name: '파주시', value: 13 },
-  //     { label: '광명시', name: '광명시', value: 14 },
-  //     { label: '김포시', name: '김포시', value: 15 },
-  //     { label: '군포시', name: '군포시', value: 16 },
-  //     { label: '광주시', name: '광주시', value: 17 },
-  //     { label: '이천시', name: '이천시', value: 18 },
-  //     { label: '양주시', name: '양주시', value: 19 },
-  //     { label: '오산시', name: '오산시', value: 20 },
-  //     { label: '구리시', name: '구리시', value: 21 },
-  //     { label: '안성시', name: '안성시', value: 22 },
-  //     { label: '포천시', name: '포천시', value: 23 },
-  //     { label: '의왕시', name: '의왕시', value: 24 },
-  //     { label: '하남시', name: '하남시', value: 25 },
-  //     { label: '여주시', name: '여주시', value: 26 },
-  //     { label: '양평군', name: '양평군', value: 27 },
-  //     { label: '동두천시', name: '동두천시', value: 28 },
-  //     { label: '과천시', name: '과천시', value: 29 },
-  //     { label: '가평군', name: '가평군', value: 30 },
-  //     { label: '연천군', name: '연천군', value: 31 },
-  //   ],
-  // }
-
-  const [locSds, setLocSds] = React.useState(locSdData[0].value)
-  const [locSkks, setLocSkks] = React.useState(locSkkData[locSds][0].value)
-
-  const handleLocSdChange = (value) => {
-    setLocSds(value)
-  }
-
-  const handleLocSkkChange = (value) => {
-    setLocSkks(value)
-  }
-
-  // style
-  const Flexbox = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    h2 {
-      font-size: 1.8rem;
-      margin: 5rem;
-    }
-  `
-
-  const SignUpSection = styled.div`
-    Input {
-      width: 80%;
-    }
-    Button {
-      width: 20%;
-    }
-    padding-bottom: 20%;
-  `
-
-  const InputData = styled.div`
-    display: flex;
-    flex-direction: column;
-  `
-  const Birth = styled.div``
-
-  const Nickname = styled.div`
-    display: flex;
-  `
   return (
     <>
       <Flexbox>
         <SignUpSection>
           <InputData>
             <h2>회원가입</h2>
+            <Nickname>
+              <Form.Item name="nickname" initialValue={nickInput}>
+                <Input
+                  type="text"
+                  name="nickname"
+                  placeholder="닉네임"
+                  style={{ width: 240 }}
+                />
+              </Form.Item>
+              <Button
+                height={'32px'}
+                width={'80px'}
+                onChange={handleNick}
+                htmlType="button"
+                Outlined
+                style={{ fontSize: '12px', fontWeight: '400' }}
+                // onClick={() => {
+                //   confirmNick()
+                // }}
+                onClick={confirmNick}
+              >
+                중복확인
+              </Button>
+            </Nickname>
+
             <Form onFinish={onFinish} form={form}>
               <Form.Item
                 name="locSd"
@@ -351,7 +334,7 @@ const SignUpView = () => {
                 </Birth>
               </Form.Item>
 
-              <Form.Item
+              {/* <Form.Item
                 name="nickName"
                 rules={[
                   { required: true, message: '닉네임을 선택해주세요' },
@@ -361,6 +344,8 @@ const SignUpView = () => {
                 <Nickname>
                   <Input type="text" name="nickName" placeholder="닉네임" />
                   <Button
+                    // onChange={handleNick}
+                    htmlType="button"
                     Secondary
                     style={{ fontSize: '12px', fontWeight: '400' }}
                     onClick={() => {
@@ -370,7 +355,7 @@ const SignUpView = () => {
                     중복확인
                   </Button>
                 </Nickname>
-              </Form.Item>
+              </Form.Item> */}
 
               <Form.Item>
                 <Button style={{ width: 300 }} htmlType="submit">
